0f797d3518d66dda88e3615ca74c7e1938caf2ec
Takao Nakaguchi <takao.nakaguchi@gmail.com>
2021-10-30 17:01:12 +0900

    madoiライブラリを最新化

--- src/madoi/madoi.d.ts
+++ src/madoi/madoi.d.ts
@@ -18,14 +18,40 @@ export declare function GetState(config?: GetStateConfig): (target: any, name: s
 export interface SetStateConfig {
 }
 export declare function SetState(config?: SetStateConfig): (target: any, name: string, descriptor: PropertyDescriptor) => void;
+export interface EnterRoomConfig {
+}
+export declare function EnterRoom(config?: EnterRoomConfig): (target: any, name: string, descriptor: PropertyDescriptor) => void;
+export interface LeaveRoomConfig {
+}
+export declare function LeaveRoom(config?: LeaveRoomConfig): (target: any, name: string, descriptor: PropertyDescriptor) => void;
+export interface PeerJoinConfig {
+}
+export declare function PeerJoin(config?: PeerJoinConfig): (target: any, name: string, descriptor: PropertyDescriptor) => void;
+export interface PeerLeaveConfig {
+}
+export declare function PeerLeave(config?: PeerLeaveConfig): (target: any, name: string, descriptor: PropertyDescriptor) => void;
 export interface MethodConfig {
     share?: ShareConfig;
     getState?: GetStateConfig;
     setState?: SetStateConfig;
+    enterRoom?: EnterRoomConfig;
+    leaveRoom?: LeaveRoomConfig;
+    peerJoin?: PeerJoinConfig;
+    peerLeave?: PeerLeaveConfig;
 }
 interface PeerInfo {
-    peerId: string;
+    id: string;
     order: number;
+}
+export interface MethodAndConfigParam {
+    method: Function;
+    share?: ShareConfig;
+    getState?: GetStateConfig;
+    setState?: SetStateConfig;
+    enterRoom?: EnterRoomConfig;
+    leaveRoom?: LeaveRoomConfig;
+    peerJoin?: PeerJoinConfig;
+    peerLeave?: PeerLeaveConfig;
 }
 export declare class Madoi {
     private connecting;
@@ -34,6 +60,10 @@ export declare class Madoi {
     private sharedObjects;
     private getStateMethods;
     private setStateMethods;
+    private enterRoomMethods;
+    private leaveRoomMethods;
+    private peerJoinMethods;
+    private peerLeaveMethods;
     private promises;
     private changedObjects;
     private handlers;
@@ -53,16 +83,20 @@ export declare class Madoi {
     onClose(_e: Event): void;
     onError(_e: Event): void;
     onElse(_msg: any): void;
-    onEnterRoom(selfId: string, peerIds: PeerInfo[]): void;
+    onEnterRoom(selfPeerId: string, peerIds: PeerInfo[]): void;
     onLeaveRoom(): void;
     onPeerJoin(peerId: string): void;
     onPeerLeave(peerId: string): void;
     send(type: string, body: object, headers: object): void;
+    broadcast(type: string, body: object, headers: object): void;
+    othercast(type: string, body: object, headers: object): void;
+    setHandler(type: string, handler: (body: any) => void): void;
+    clearHandler(type: string): void;
     sendMessage(msg: object): void;
-    register(obj: any, methodAndConfigs?: [Function, MethodConfig][]): void;
-    registerFunction(func: Function, config: ShareConfig): () => any;
+    register(obj: any, methodAndConfigs?: MethodAndConfigParam[]): void;
+    registerShareFunction(func: Function, config?: ShareConfig): () => any;
     private addSharedFunction;
-    private saveStates;
+    saveStates(): void;
     private applyInvocation;
 }
 export {};
